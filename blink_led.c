3115ffa5bebd5edaf47b6f5849018a3a4079ba14
terryspitz <terryspitz@gmail.com>
2021-04-30 17:22:08 +0100

    Minute roller rotation in steps of 1/24 rev, with pause

--- blink_led.c
+++ blink_led.c
@@ -1,56 +1,64 @@
-// Copyright (C) 2014 Diego Herranz
+// Copyright (C) 2021 terryspitz@gmail
 
 #define NO_BIT_DEFINES
 #include <pic16f88.h>
 #include <stdint.h> 
 
-// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN),
-// disable watchdog,
-// and disable low voltage programming.
-// The rest of fuses are left as default.
-__code uint16_t __at (_CONFIG1) __configword = _INTRC_IO & _WDTE_OFF & _LVP_OFF & _MCLRE_OFF;
+__code uint16_t __at (_CONFIG1) __configword = 
+    _INTRC_IO         // internal oscillator, allow IO port
+    & _WDTE_OFF     // disable watchdog timer
+    & _LVP_OFF         // disable low voltage programming.
+    & _MCLRE_OFF;    // disable reset
 
-#define LED_TRIS TRISAbits.TRISA1
-#define LED_PORT PORTAbits.RA1
-
-
-static void Timer1Delay(void){  //1ms delay
-    T1CON=0x00;                      //Timer-1 internal clock, 1:1 prescale
-    // TMR1H=256-1000/256;        //Count Hight Byte
-    TMR1H=252;        //Count Hight Byte
-    TMR1L=256-232;                      //Count Low Byte
-	PIR1bits.TMR1IF = 0;	         //Clear flag
-    T1CONbits.TMR1ON=1;              //Run timer
-    while(PIR1bits.TMR1IF==0);       //Wait for flag to over flow
-    T1CONbits.TMR1ON=0;              //Switch off timer
+static void delay(uint16_t ticks){  
+    T1CON = 0x00;                   //Timer-1 internal clock, 1:1 prescale
+    TMR1H = 0xFF - ticks / 256;    	//timer start Hight Byte
+    TMR1L = 0xFF - ticks % 256;     //timer start Low Byte
+    PIR1bits.TMR1IF = 0;            //Clear overflow flag
+    T1CONbits.TMR1ON = 1;           //Run timer
+    while(PIR1bits.TMR1IF == 0);    //Wait for flag to overflow
+    T1CONbits.TMR1ON = 0;           //Switch off timer
 }
 
-static void delay(uint16_t ms)
-{
-	for (uint16_t i = ms; i>0; --i) {
-		// for (char j = 250; j > 0; --j) {
-		// 	__asm nop __endasm;  // Prevent this loop from being optimized away.
-		// }
-		Timer1Delay();
-	}
-}
-
-static const uint16_t DELAY_MS = 10;
+#define LED_TRIS TRISAbits.TRISA2
+#define LED_PORT PORTAbits.RA2
+#define STEP_TRIS TRISAbits.TRISA1
+#define STEP_PORT PORTAbits.RA1
+#define SLEEP_TRIS TRISAbits.TRISA0
+#define SLEEP_PORT PORTAbits.RA0
+#define BUTTON_TRIS TRISBbits.TRISB3
+#define BUTTON_PORT PORTBbits.RB3
 
 void main(void)
 {
-	CMCON=7;         // Comparators off, all pins digital I/O 
-	LED_PORT = 1; // LED 
-	LED_TRIS = 0; // Pin as output
-	LED_PORT = 1; // LED 
-
-	while (1) {
-		delay(DELAY_MS);
-		// LED_PORT = 0;
-		LED_PORT = 1;
-		delay(DELAY_MS);
-		// LED_PORT = 1;
-		LED_PORT = 0;
-		// __asm CLRWDT __endasm;  //clear watchdog timer, or disable in config above
-	}
+    CMCON = 7;               // Comparators off, all pins digital I/O 
+    OPTION_REGbits.NOT_RBPU = 0; //Enable RB Pull-ups
+    __asm nop __endasm;      // NOP, pause while comparators settle
+    LED_TRIS = 0;            // output
+    STEP_TRIS = 0;           // output
+    SLEEP_TRIS = 0;          // output
+    BUTTON_TRIS = 1;         // input
+	const uint16_t delay_ticks = 80;
+	const uint16_t ticks_per_min = 20 * 150 /30*31; // 60*2 * 
+	_Bool led = 0;
+	_Bool step = 0;
+    while (1) {
+		SLEEP_PORT = 1;	 //enable stepper
+		for(uint16_t t = ticks_per_min; t; --t) {
+			step = !step;
+			STEP_PORT = step;
+			delay(delay_ticks);
+		}
+		SLEEP_PORT = 0;	 //sleep stepper
+        if(!BUTTON_PORT) {
+            while(!BUTTON_PORT) {}
+        }
+		else {
+			//pause
+			for(uint16_t t = ticks_per_min*4; t; --t)
+				delay(delay_ticks);
+		}
+		led = !led;
+        LED_PORT = led;
+    }
 }
